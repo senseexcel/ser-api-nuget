2080095af64d92507f33362c632cc64c08dd245c
Martin Berthold <martin.berthold@akquinet.de>
2018-03-07 09:41:19 +0100

    Change naming in Namespace

--- src/AppConfig.cs
+++ src/AppConfig.cs
@@ -7,7 +7,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  */
 #endregion
 
-namespace SerApiNuget
+namespace SerApi
 {
     #region Usings
     using Newtonsoft.Json;
