e84b2fb427ba33eb328fb21ea0b8982a9037dabb
MBerthold <MBerthold@NB-FC-215335>
2021-04-29 10:48:39 +0200

    Add new rest status

---
+++ src/RestTaskStatus.cs
@@ -0,0 +1,34 @@
+﻿namespace Ser.Engine.Rest.Model
+{
+    #region Usings
+    using System;
+    #endregion
+
+    /// <summary>
+    /// Task information
+    /// </summary>
+    public class RestTaskStatus
+    {
+        #region Properties
+        /// <summary>
+        /// Status
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Task State Message
+        /// </summary>
+        public string ProcessMessage { get; set; }
+
+        /// <summary>
+        /// DistributeResult
+        /// </summary>
+        public string DistributeResult { get; set; }
+
+        /// <summary>
+        /// Job result list as Json
+        /// </summary>
+        public string JobResultJson { get; set; }
+        #endregion
+    }
+}
