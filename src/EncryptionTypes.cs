9f6a94b10d144efc59d6b83d7ea41dd0c196dffd
Martin Berthold <bertholm@draeger.com>
2022-09-14 08:00:17 +0200

    new encryption types

---
+++ src/EncryptionTypes.cs
@@ -0,0 +1,24 @@
+﻿namespace AgApi
+{
+    #region Usings
+    using System;
+    using System.Text.Json.Serialization;
+    #endregion
+
+    /// <summary>
+    /// Encryption types
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum EncryptionType
+    {
+        /// <summary>
+        /// RSA 256 Bit encryption
+        /// </summary>
+        RSA256 = 0,
+
+        /// <summary>
+        /// AES 256 Bit encryption
+        /// </summary>
+        AES256 = 1
+    }
+}
